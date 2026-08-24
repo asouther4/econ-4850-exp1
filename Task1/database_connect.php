4cac5169ca11d4cdfa531bd32d1dde48b3743ba4
Andrew Souther <asouther@fordham.edu>
2026-08-24 15:29:55 -0400

    add Silvio's draft of experiment code

---
+++ Task1/database_connect.php
@@ -0,0 +1,12 @@
+<?php
+
+//$dbc = mysql_connect('mysql8.000webhost.com', 'a9458604_decisio', 'silvio');
+//mysql_select_db('a9458604_decisio', $dbc);
+
+//$dbc = mysql_connect('127.0.0.1', 'root', '');
+//mysql_select_db('decison', $dbc);
+
+$dbc = mysql_connect('31.220.17.250', 'root', '');
+mysql_select_db('data', $dbc);
+
+?>
